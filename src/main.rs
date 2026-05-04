50dc4005d630a434ba35a760eaf2f8d77e0ae262
Swapnil Chauhan <swapnilchauhan999@gmail.com>
2026-05-04 13:55:13 +0100

    upto 3.8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+use newsletter_server::configuration::get_configuration;
+use newsletter_server::startup::run;
+
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
